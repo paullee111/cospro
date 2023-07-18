bd4e03c555a68c16f92c7f6e08207c26333e606d
paullee111 <132044882+paullee111@users.noreply.github.com>
2023-07-18 13:09:36 +0900

    Update cospro-002.c

--- cospro-002/cospro-002.c
+++ cospro-002/cospro-002.c
@@ -93,28 +93,28 @@ int main()
 #include <string.h>
 int solution(int programs[][2], int programs_len)
 {
-
-	int answer = 0;
-	int count=0;
-	int used_tv[25] = { 0, };
-
-	
-	answer++;
-
-
+    int answer = 0;
+    int used_tv[25] = { 0, };
+    for (int i = 0; i < programs_len; i++) {
+        for (int j = programs[i][0]; j < programs[i][1]; j++) {
+            used_tv[j]++;
+        }
+    }
+    for (int i = 0; i < 25; i++) {
+        if (used_tv[i] >= 2) {
+            answer++;
+        }
+    }
 return answer;
 }
-
 int main() {
 
-		int programs[][2] = { {1,6},{3,5},{2,8},{4,7} };
+        int programs[4][2] = { {1,6},{3,5},{2,8},{4,7} };
 
-	int programs_len = 4;
+    int programs_len = 4;
 
-	int ret = solution(programs, programs_len);
+    int ret = solution(programs, programs_len);
 
-	printf("solution %d\n", ret);
-
-		return 0;
-
+    printf("solution %d\n", ret);
+        return 0;
 }
